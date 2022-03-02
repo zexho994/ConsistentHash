bef902994b1cedce70468a86bc31b3d4c024254f
zexho <zouzhihao994@gmail.com>
2022-03-02 19:20:37 +0800

    update(v1.0.0): 添加接口对象

---
+++ src/ConsistentHashManager.java
@@ -0,0 +1,25 @@
+/**
+ * @author Zexho
+ * @date 2022/3/2 7:05 PM
+ */
+public class ConsistentHashManager {
+
+    public ConsistentHashManager() {
+
+    }
+
+    public void addNode() {
+
+    }
+
+    public void removeNote() {
+
+    }
+
+    public Node getNextNote(int hash) {
+
+        return null;
+    }
+
+
+}
